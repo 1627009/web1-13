74c39318f970f097746b90c4a340cab2dc06ff8a
1627009 <g1627009@st.t-kougei.ac.jp>
2017-07-03 17:31:57 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,3 +1,15 @@
+function getFileName(){
+  return window.location.href.split('/').pop();
+}
+
+var filename = getFileName();
+var opt;
+id(filename === 'other.html')
+  opt = document.querySelecter('option[value="other.html"]');
+else
+  opt = document.querySelecter('option[value="index.html"]');
+opt.selected = true;
+
 document.getElementById('form').select.onchange = function(){
   location.href = document.getElementById('form').select.value;
 }
